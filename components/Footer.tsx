e7617193d5d95acdccdf12c1dd40568eca1e313f
nelsonchoo456 <nelsonchoo456@gmail.com>
2023-08-03 15:32:40 +0800

    edited links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,14 +15,18 @@ const Footer = () => {
           © 2023 Nelson Choo<a href="/" className="hover:underline"></a>
         </div>
         <div className="flex flex-row items-center justify-center space-x-4 mb-1">
-          <a href="https://github.com/hqasmei" rel="noreferrer" target="_blank">
+          <a
+            href="https://github.com/nelsonchoo456"
+            rel="noreferrer"
+            target="_blank"
+          >
             <AiOutlineGithub
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
               size={30}
             />
           </a>
           <a
-            href="https://www.linkedin.com/in/hosnaqasmei/"
+            href="https://www.linkedin.com/in/nelson-choo-jia-wei/"
             rel="noreferrer"
             target="_blank"
           >
